7af2a1c8ae7cfa4430070eacfc637b6b5f3236f7
Prashant Gupta <prashantgupta@us.ibm.com>
2018-05-05 11:04:48 -0700

    adding jupyter notebook

--- src/car_info_scraper.py
+++ src/car_info_scraper.py
@@ -2,14 +2,20 @@ import pandas as pd
 
 from gather_info import gatherInfoFromURL
 
+
 def scraper():
-    carinfo = gatherInfoFromURL(url="http://www.cars-data.com/en/sport-cars.html")
+    carinfo = gatherInfoFromURL(
+        url="http://www.cars-data.com/en/sport-cars.html")
     #carInfo = {"cars": [], "year": [], "power": [], "carType": []}
 
     print(len(carinfo["cars"]))
 
-    carDataFrame = pd.DataFrame({"Cars": carinfo.get("cars")})
-    print(carDataFrame.info())
+    carDataFrame = pd.DataFrame(
+        {"Cars": carinfo.get("cars"), "year": carinfo.get("year"), "power": carinfo.get("power"), "car type": carinfo.get("carType")})
+
+    print(carDataFrame.head())
+    print(carDataFrame.shape)
+
 
 if __name__ == '__main__':
     scraper()
